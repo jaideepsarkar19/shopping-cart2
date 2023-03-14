f7380d691991c2c0fb59fe4d578526ebdb46f8fa
jaideepsarkar19 <jaideep.sarkar19@rediffmail.com>
2023-03-14 11:53:03 +0530

    2nd

--- src/component/Section.js
+++ src/component/Section.js
@@ -1,6 +1,6 @@
-import react, { useState } from "react";
+import React from "react";
 import "../css/styles.css";
-import ProductCard from "./ProductCard";
+import {useEffect,useState} from "react";
 import ProductsJSON from "../Products.json";
 
 export default function Section(props) {
@@ -15,6 +15,16 @@ export default function Section(props) {
     var count = Products.filter((i) => i.isAddedToCart == true).length;
     props.addToCart(count);
   };
+  const [productData, setProductData] = useState({});
+  useEffect(() => {
+    // console.log("useeffect: ", props.productData);
+    setProductData(productData);
+  }, []);
+
+  // const setArrayValue = (id) => {
+  //   console.log("ID-->, ", id);
+  //   props.setArrayValue(id);
+  // };
 
   return (
     <div>
@@ -25,7 +35,7 @@ export default function Section(props) {
             {Products.map((i, v) => (
               <>
                 {/* {console.log("Section:", i, v)} */}
-                <ProductCard productData={i} setArrayValue={setArrayValue} />
+                {/* <ProductCard productData={i} setArrayValue={setArrayValue} /> */}
               </>
             ))}
           </div>
@@ -34,3 +44,60 @@ export default function Section(props) {
     </div>
   );
 }
+
+const ProductCard = (props) => {
+
+  return (
+    <>
+      {/* {console.log("ProductCard: ", ProductCard)} */}
+      <div className="col mb-5">
+        <div className="card h-100">
+          <div
+            className="badge bg-dark text-white position-absolute"
+            style={{ top: "0.5rem", right: "0.5rem" }}
+          >
+            Sale
+          </div>
+
+          <img
+            className="card-img-top"
+            src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+            alt="..."
+          />
+
+          <div className="card-body p-4">
+            <div className="text-center">
+              <h5 className="fw-bolder">
+                {productData.Name} {productData.id}
+              </h5>
+              <h5 className="fw-bolder">Special Item</h5>
+              <div className="d-flex justify-content-center small text-warning mb-2">
+                <div className="bi-star-fill"></div>
+                <div className="bi-star-fill"></div>
+                <div className="bi-star-fill"></div>
+                <div className="bi-star-fill"></div>
+                <div className="bi-star-fill"></div>
+              </div>
+              <span className="text-muted text-decoration-line-through">
+                $20.00
+              </span>
+              $18.00
+            </div>
+          </div>
+
+          <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div className="text-center">
+              <button
+                className="btn btn-outline-dark mt-auto"
+                onClick={() => setArrayValue(productData.id)}
+              >
+                {productData.isAddedToCart ? "Remove from cart" : "Add to cart"}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
